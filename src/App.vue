b845d217ed2f5888c023584cebf1536b6ec51ff9
ErginTIRAVOGLU <ergintiravoglu@msn.com>
2024-12-16 10:10:35 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import { RouterView } from 'vue-router';
+
+import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
+
+
+ 
+
+
+</script>
+<template>
+     <Navbar />
+     <RouterView />
+     <!--<Hero  />-->
+     <!--<Hero title="Test Title" subTitle="Test SubTitle" />-->
+     <!--<HomeCards />-->
+     <!--<JobListings :limit="3" :show-button="true" />-->
+</template>
---
+++ src/App2.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: 'notactive',
+      tasks: [
+        'Task One', 'Task Two', 'Task Three'
+      ],
+      link: 'https://www.google.com'
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = 'inactive';
+      } else {
+        this.status = 'active';
+      }
+    }
+  }
+}
+
+</script>
+
+<template>
+  <h1>Vue Jobs {{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" v-bind:key="task">{{ task }}</li>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <a v-bind:href="link">Click me</a>
+  <a :href="link">Click me</a>
+  <br />
+  <button v-on:click="toggleStatus">Change Status</button>
+  <button @click="toggleStatus">Change Status</button>
+</template>
